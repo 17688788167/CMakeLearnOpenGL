c1034123e87b16cf56e834cfdbfc23198e389dac
xia-luhui <xialuhui@163.com>
2022-07-22 00:46:33 +0800

    实现以actor为单位绘制

--- environment/learnopengl/Actor.h
+++ environment/learnopengl/Actor.h
@@ -13,35 +13,25 @@ class Actor
 {
 public:
 	Scene* scene;
-
-
-	unsigned int VBO, cubeVAO;
-
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+	unsigned int ActorVBO, ActorVAO;
+	float *ActorVertices;
+	
 
 
 	void DrawActor()
 	{
-		glGenBuffers(1, &VBO);
+		glBindVertexArray(ActorVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 	}
 
-	Actor()
+
+	
+	Actor(float Scale,float xOffset,float yOffset)
 	{
-		
-		glGenVertexArrays(1, &cubeVAO);
-		glGenBuffers(1, &VBO);
+		ActorVerticeData(Scale, xOffset, yOffset);
 
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Data::vertices), Data::vertices, GL_STATIC_DRAW);
-
-		glBindVertexArray(cubeVAO);
-
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-
-
-
-
+		BindVAOVBO();
 	}
 
 	virtual ~Actor()
@@ -50,7 +40,47 @@ public:
 		{
 			delete(scene);
 		}
+		ActorVertices = nullptr;
+	}
+private:
+
+	void BindVAOVBO()
+	{
+		glGenVertexArrays(1, &ActorVAO);
+		glGenBuffers(1, &ActorVBO);
+
+		glBindVertexArray(ActorVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, ActorVBO);
+		cout << sizeof(ActorVertices) << endl;
+		cout << sizeof(Data::vertices) << endl;
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Data::vertices), ActorVertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
 	}
 
+	void ActorVerticeData(float Scale, float xOffset, float yOffset)
+	{
+		int num = sizeof(Data::vertices) / sizeof(float);
 
+		ActorVertices = new float[num];
+		for (int i = 0; i < num; ++i)
+		{
+			ActorVertices[i] = Data::vertices[i] * Scale;
+			if (i % 3 == 0)
+			{
+				ActorVertices[i] += xOffset;
+				//cout << ActorVertices[i]<<", ";
+			}
+			else if (i % 3 == 1)
+			{
+				ActorVertices[i] += yOffset;
+				//cout << ActorVertices[i] << ", ";
+			}
+			else if (i % 3 == 2)
+			{
+				//cout << ActorVertices[i] << endl;
+			}
+		}
+	}
 };
